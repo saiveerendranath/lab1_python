62afbcaede05f9789b936fdc3d515e16aa88b5f5
saiveerendranath <40311376+saiveerendranath@users.noreply.github.com>
2020-01-04 13:12:55 +0530

    Add files via upload

---
+++ sort_array.py
@@ -0,0 +1,24 @@
+
+
+numbers = [1, 3, 4, 2] 
+  
+
+numbers.sort() 
+  
+print(numbers) 
+  
+ 
+decimalnumber = [2.01, 2.00, 3.67, 3.28, 1.68] 
+  
+
+decimalnumber.sort() 
+  
+print(decimalnumber) 
+  
+ 
+words = ["Geeks", "For", "Geeks"] 
+  
+ 
+words.sort() 
+  
+print(words) 
